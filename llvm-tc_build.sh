0b7ce1f48863c74e6042f922d9782961cda22a01
Fahmi Nurul Huda <43783842+tolaylanz@users.noreply.github.com>
2021-08-04 20:04:01 +0700

    Update llvm-tc_build.sh

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/kdrag0n/proton-clang $(pwd)/llvmTC -b llvm-tc
-cd $(pwd)/llvmTC
+git clone https://github.com/kdrag0n/proton-clang $(pwd)/proton-clang -b llvm-tc
+cd $(pwd)/proton-clang
 bash build-tc.sh
